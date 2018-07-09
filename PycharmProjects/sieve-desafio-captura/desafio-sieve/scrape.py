1662efb88c2060f858dd50ba908386848bea6f7e
Mariana Medeiros Ruddy Santos <mariana.ruddy@b2wdigital.com>
2018-07-09 18:57:21 -0300

    ajuda do diogo

--- PycharmProjects/sieve-desafio-captura/desafio-sieve/scrape.py
+++ PycharmProjects/sieve-desafio-captura/desafio-sieve/scrape.py
@@ -7,20 +7,30 @@ source = requests.get('https://www.epocacosmeticos.com.br').text
 soup = BeautifulSoup(source,'lxml')
 # print(soup.prettify())
 
-for link in soup.find_all('a'):
-    # if 'https://www.epocacosmeticos.com.br' in link['href']:
-        # print(link['href'])
+def get_product_pages(soup_):
+    products = set()
+    for link in soup_.find_all('a'):
+        # if 'https://www.epocacosmeticos.com.br' in link['href']:
+            # print(link['href'])
 
-    if 'https://www.epocacosmeticos.com.br' in link['href']:
-        linksplitted = link['href'].split('/')
-        if link['href'].split('/')[-1:] == ['p']:
-            print(link['href'])
-            linkPagProd = requests.get(link['href']).text
-            soupPagProd = BeautifulSoup(linkPagProd,'lxml')
-            # print(soupPagProd.prettify())
-            for nomeProd in soupPagProd.find_all('a',attrs={"name": "Abstract"}):
-                print(nomeProd['content'])
+        if 'https://www.epocacosmeticos.com.br' in link['href']:
+            if link['href'].endswith('/p'):
+                products.add(link['href'])
+    return products
 
 
+for url in get_product_pages(soup):
+    print(url)
+    linkPagProd = requests.get(url).text
+    soupPagProd = BeautifulSoup(linkPagProd,'lxml')
+    # print(soupPagProd.prettify())
+    for nomeProd in soupPagProd.find_all('div',class_="productName"):
+        print(nomeProd.text)
 
-# <meta content="Corretivo para Olhos Maybelline Instant Age Eraser" name="Abstract"/>
+
+result = []
+for link in iteravel:
+    result.append(link['href'])
+
+
+result = [link['href'] for link in iteravel]
