3ceb39fdeeed84e21abe1679f5d158b83bd72390
Mariana Medeiros Ruddy Santos <mariana.ruddy@b2wdigital.com>
2018-07-09 17:57:35 -0300

    printando url e nome do produto

--- PycharmProjects/sieve-desafio-captura/desafio-sieve/scrape.py
+++ PycharmProjects/sieve-desafio-captura/desafio-sieve/scrape.py
@@ -4,9 +4,7 @@ from bs4 import BeautifulSoup
 import requests
 
 source = requests.get('https://www.epocacosmeticos.com.br').text
-
 soup = BeautifulSoup(source,'lxml')
-
 # print(soup.prettify())
 
 for link in soup.find_all('a'):
@@ -17,3 +15,12 @@ for link in soup.find_all('a'):
         linksplitted = link['href'].split('/')
         if link['href'].split('/')[-1:] == ['p']:
             print(link['href'])
+            linkPagProd = requests.get(link['href']).text
+            soupPagProd = BeautifulSoup(linkPagProd,'lxml')
+            # print(soupPagProd.prettify())
+            for nomeProd in soupPagProd.find_all('div',class_=""):
+                print(nomeProd['content'])
+
+
+
+# <meta content="Corretivo para Olhos Maybelline Instant Age Eraser" name="Abstract"/>
